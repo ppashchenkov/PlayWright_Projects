1899a612cb9fbc10bfb62ea2856a4d168b0d592d
pavel <ppashchenkov@gmail.com>
2023-10-07 23:35:03 +0300

    Add 3 test classes

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -15,23 +15,5 @@ public class App {
             page.navigate("https://playwright.dev/");
             page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("example.png")));
         }
-        try (Playwright playwright = Playwright.create()) {
-            Browser browser = playwright.firefox().launch(
-                    new BrowserType.LaunchOptions().setHeadless(true)
-            );
-            Page page = browser.newPage();
-            page.navigate("https://openweathermap.org");
-//            String title = page.title();
-//            System.out.println(title);
-//            assertThat(page).hasTitle("Сurrent weather and forecast - OpenWeatherMap");
-            Locator currTemp = page.locator("css=.current-temp .heading");
-            String currentTemp = currTemp.innerText();
-
-            System.out.println(currentTemp);
-
-            assertThat(currTemp).isVisible();
-            assertThat(currTemp).containsText("C");
-
-        }
     }
 }
---
+++ src/main/java/PlayWRT.java
@@ -0,0 +1,28 @@
+import com.microsoft.playwright.*;
+
+import java.nio.file.Paths;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+
+public class PlayWRT {
+    public static void main(String[] args) {
+        try (Playwright playwright = Playwright.create()) {
+            Browser browser = playwright.firefox().launch(
+                    new BrowserType.LaunchOptions().setHeadless(true)
+            );
+            Page page = browser.newPage();
+            page.navigate("https://openweathermap.org");
+//            String title = page.title();
+//            System.out.println(title);
+//            assertThat(page).hasTitle("Сurrent weather and forecast - OpenWeatherMap");
+            Locator currTemp = page.locator("css=.current-temp .heading");
+            String currentTemp = currTemp.innerText();
+
+            System.out.println(currentTemp);
+
+            assertThat(currTemp).isVisible();
+            assertThat(currTemp).containsText("C");
+
+        }
+    }
+}
