3f4b4ec7c59ee51ec9e15d7870d0fd971d3f9448
ppashchenkov <ppashchenkov@gmail.com>
2024-01-15 18:39:35 +0300

    First two tests in NavigationTest

--- src/test/java/luma/NavigationTest.java
+++ src/test/java/luma/NavigationTest.java
@@ -1,24 +1,36 @@
 package luma;
 
 import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.options.AriaRole;
 import luma.runner.BaseTest;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 
-public class NavigationTest extends BaseTest {
+public class NavigationTest {
+//    final String baseURL = "https://magento.softwaretestingboard.com/";
+    final String baseURL = "https://naveenautomationlabs.com/opencart/";
+    Playwright playwright = Playwright.create();
+    Browser browser = playwright.chromium().launch(new BrowserType
+            .LaunchOptions().setHeadless(true).setSlowMo(100));
+    Page page = browser.newPage();
 
-    String baseURL = "https://magento.softwaretestingboard.com/";
-
-    public void baseUrlTest() {
-
-        Playwright playwright = Playwright.create();
-        Browser browser = playwright.chromium().launch();
-        Page page = browser.newPage();
+    @Test
+    public void testBaseUrlLanding() {
         page.navigate(baseURL);
         assertThat(page).hasURL(baseURL);
+    }
+
+    @Test
+    public void testFor() {
+        page.navigate(baseURL);
+        page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions()
+                .setName("Tablets").setExact(true)).click();
+
+        assertThat(page).hasURL("https://naveenautomationlabs.com/opencart/index.php?route=product/category&path=57");
     }
 }
