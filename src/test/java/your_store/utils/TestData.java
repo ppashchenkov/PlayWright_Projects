2967028df436703b62a861fb8c8cf13acc53d883
ppashchenkov <ppashchenkov@gmail.com>
2024-02-07 11:39:25 +0300

    Added Software menu test

--- src/test/java/your_store/utils/TestData.java
+++ src/test/java/your_store/utils/TestData.java
@@ -8,5 +8,7 @@ public final class TestData {
     public static final String TABLETS_MENU_END_POINT = "/index.php?route=product/category&path=57";
 
     public static final String TABLETS_MENU = "Tablets";
+    public static final String SOFTWARE_MENU_AND_POINT = "/index.php?route=product/category&path=17";
+    public static final String SOFTWARE_MENU = "Software";
 
 }
