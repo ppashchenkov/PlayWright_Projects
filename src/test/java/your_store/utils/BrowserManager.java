5c0d0c0e82ed5ec891a42848305721e1e9fd19f1
ppashchenkov <ppashchenkov@gmail.com>
2024-03-10 20:05:49 +0300

    Meeting at 10 March ( ConfigProperties.java )

--- src/test/java/your_store/utils/BrowserManager.java
+++ src/test/java/your_store/utils/BrowserManager.java
@@ -4,23 +4,32 @@ import com.microsoft.playwright.Browser;
 import com.microsoft.playwright.BrowserType;
 import com.microsoft.playwright.Playwright;
 
+import java.util.Map;
+
 public final class BrowserManager {
 
-    public static Browser createBrowser(Playwright playwright, String browserName, boolean isHeadLess, int slowMo) {
+    public static Browser createBrowser(Playwright playwright, Map<String,String> enviroment) {
+        String browserName = enviroment.get("browser");
+        boolean isHeadLess = Boolean.parseBoolean(enviroment.get("isHeadLess"));
+        int slowMo = Integer.parseInt(enviroment.get("slowMo"));
+
         switch (browserName) {
             case "chromium" -> {
+                LoggerUtils.logInfo("INFO: " + browserName + " launched.");
                 return playwright.chromium()
                         .launch(new BrowserType.LaunchOptions()
                                 .setHeadless(isHeadLess)
                                 .setSlowMo(slowMo));
             }
             case "firefox" -> {
+                LoggerUtils.logInfo("INFO: " + browserName + " launched.");
                 return playwright.firefox()
                         .launch(new BrowserType.LaunchOptions()
                                 .setHeadless(isHeadLess)
                                 .setSlowMo(slowMo));
             }
             case "webkit" -> {
+                LoggerUtils.logInfo("INFO: " + browserName + " launched.");
                 return playwright.webkit()
                         .launch(new BrowserType.LaunchOptions()
                                 .setHeadless(isHeadLess)
