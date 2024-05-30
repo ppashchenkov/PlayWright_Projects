8db45af64126691244bae778fb34d3af7b95c1f0
ppashchenkov <ppashchenkov@gmail.com>
2024-05-30 15:05:42 +0300

    Restore to new working configuration

--- src/test/java/your_store/utils/TestData.java
+++ src/test/java/your_store/utils/TestData.java
@@ -1,7 +1,6 @@
 package your_store.utils;
 
 public final class TestData {
-//    public static final String BASE_URL = "https://magento.softwaretestingboard.com/";
     public static final String BASE_URL = "https://naveenautomationlabs.com/opencart";
     public static final String HOME_END_POINT = "/";
     public static final String TABLETS_MENU_END_POINT = "/index.php?route=product/category&path=57";
